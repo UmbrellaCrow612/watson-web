c57ce936faf60e4c67d5c63b789d51ea0eb41ff7
yousaf wazir <umbrellaman612@gmail.com>
2023-02-03 15:30:55 +0000

    added theme switcher

--- src/components/Navigation/ThemeSwitcher.tsx
+++ src/components/Navigation/ThemeSwitcher.tsx
@@ -1,22 +1,77 @@
+import { useState, useMemo, useEffect } from "react";
 import { HiOutlineColorSwatch } from "react-icons/hi";
+import { themeChange } from "theme-change";
 
 export default function ThemeSwitcher() {
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    themeChange(false);
+  }, [input]);
+
+  const filteredThemes = useMemo(() => {
+    const Themes = [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ];
+
+    return Themes.filter((t) =>
+      t.toLowerCase().includes(input.toLowerCase().trim())
+    );
+  }, [input]);
+
   return (
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost text-2xl">
-        <HiOutlineColorSwatch />
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a>Item 1</a>
-        </li>
-        <li>
-          <a>Item 2</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="text-xl btn btn-outline">
+          <HiOutlineColorSwatch />
+        </label>
+        <ul
+          tabIndex={0}
+          className="inline-block p-2 overflow-y-scroll shadow dropdown-content menu bg-base-100 rounded-box w-52 h-60"
+        >
+          <input
+            type="text"
+            placeholder="Type here"
+            className="w-full max-w-xs input input-bordered"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          {filteredThemes.map((theme) => (
+            <li key={theme}>
+              <button data-set-theme={`${theme}`} className="btn btn-ghost">
+                {theme}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
