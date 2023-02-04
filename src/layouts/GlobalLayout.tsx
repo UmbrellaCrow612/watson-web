1f1754c16502a897eedf5c614753caa8bf84962c
yousaf wazir <umbrellaman612@gmail.com>
2023-02-04 17:50:06 +0000

    fixed font flicker with next font

--- src/layouts/GlobalLayout.tsx
+++ src/layouts/GlobalLayout.tsx
@@ -1,10 +1,18 @@
 import Footer from "@/components/Footer";
 import Navigation from "@/components/Navigation";
 import { ReactNode } from "react";
+import { IBM_Plex_Mono } from "@next/font/google";
+
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600"],
+});
 
 export default function GlobalLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-screen-lg mx-auto px-6 lg:px-0">
+    <div
+      className={`max-w-screen-lg px-6 mx-auto lg:px-0 ${ibmPlexMono.className}`}
+    >
       <Navigation />
       <main>{children}</main>
       <Footer />
