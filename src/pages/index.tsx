74227a3a02abb544d27adebe4a5caeba617338e7
yousaf wazir <umbrellaman612@gmail.com>
2023-02-06 09:43:50 +0000

    changed script tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,9 @@ export default function Page() {
         }}
       >
         {`window.watsonAssistantChatOptions = {
-    integrationID: "9e1d9225-e6df-4f38-a99f-351d5418a8bd", 
+    integrationID: "27a99238-c2cb-4b65-a064-20382341bfdd", // The ID of this integration.
     region: "eu-gb", // The region your integration is hosted in.
-    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608",
+    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608", // The ID of your service instance.
     onLoad: function(instance) { instance.render(); }
   };
   setTimeout(function(){
