88cbc52a21eaec4d213dabf3df13dcb4e1e9a92e
yousaf wazir <umbrellaman612@gmail.com>
2023-02-09 09:38:39 +0000

    refactored script to be in a const

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,29 +7,18 @@ import { TbFileCertificate } from "react-icons/tb";
 import { FaRegHandshake } from "react-icons/fa";
 import CountUp from "react-countup";
 import Card from "@/components/Card";
+import { WatsonScript } from "../scripts/index";
 
 export default function Page() {
   return (
     <>
       <Script
-        id="watson"
-        strategy="lazyOnload"
-        onError={() => {
-          console.log("Watson not working index.tsx");
+        id="watson-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: WatsonScript,
         }}
-      >
-        {`window.watsonAssistantChatOptions = {
-    integrationID: "9e1d9225-e6df-4f38-a99f-351d5418a8bd", // The ID of this integration.
-    region: "eu-gb", // The region your integration is hosted in.
-    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608", // The ID of your service instance.
-    onLoad: function(instance) { instance.render(); }
-  };
-  setTimeout(function(){
-    const t=document.createElement('script');
-    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-    document.head.appendChild(t);
-  });`}
-      </Script>
+      />
       <Head>
         <title>IBM AI Skills Build</title>
         <meta
---
+++ src/scripts/index.tsx
@@ -0,0 +1,11 @@
+export const WatsonScript = `window.watsonAssistantChatOptions = {
+    integrationID: "9e1d9225-e6df-4f38-a99f-351d5418a8bd", // The ID of this integration.
+    region: "eu-gb", // The region your integration is hosted in.
+    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608", // The ID of your service instance.
+    onLoad: function(instance) { instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });`;
