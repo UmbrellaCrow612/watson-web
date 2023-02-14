54bb775a148c314eca3fb2abd9b2862816573c09
yousaf wazir <umbrellaman612@gmail.com>
2023-02-14 13:36:32 +0000

    added accessbility to widget no tab out

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,8 +1,9 @@
 export const WatsonScript = `window.watsonAssistantChatOptions = {
-    integrationID: "9e1d9225-e6df-4f38-a99f-351d5418a8bd", // The ID of this integration.
-    region: "eu-gb", // The region your integration is hosted in.
-    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608", // The ID of your service instance.
-    onLoad: function(instance) { instance.render(); }
+    integrationID: "9e1d9225-e6df-4f38-a99f-351d5418a8bd",
+    region: "eu-gb",
+    serviceInstanceID: "9105472d-0990-4acc-a349-661d4607d608",
+    onLoad: function(instance) { instance.render(); },
+    enableFocusTrap:true
   };
   setTimeout(function(){
     const t=document.createElement('script');
